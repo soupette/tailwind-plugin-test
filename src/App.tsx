cbf594f8ad9576994b969c61ca968c6cd8657926
soupette <cyril.lopez@payfit.com>
2024-08-14 14:27:45 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold underline">Hello world!</h1>
+
+      <button className="uy-px-200 uy-py-100 uy-rounded-100 uy-bg-surface-primary uy-text-content-inverted">
+        Hello
+      </button>
+    </>
+  );
+}
+
+export default App;
